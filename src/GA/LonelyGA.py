649c09f4803b0c6d43887372abf2664ba433e127
jmog <jmog@itu.dk>
2020-02-24 13:06:29 +0100

    Timer for the GARunner.py, and refactored LonelyGA, to only initialize when the start function is called

--- src/GA/LonelyGA.py
+++ src/GA/LonelyGA.py
@@ -10,8 +10,16 @@ from src.DNA.LonelyDNA import LonelyDNA
 
 class LonelyGA:
     generation_counter = 0
+    input_shape = None
+    output_size = None
+    dataset = None
+    scaling = None
+    population_size = None
+    epochs = None
+    matingpool = None
+    population = None
 
-    def __init__(self, input_shape, output_shape, initial_max_nodes, activation, optimizer, loss, population_size, mutation_rate, dataset, scaling, epochs, matingpool):
+    def start(self, input_shape, output_shape, initial_max_nodes, activation, optimizer, loss, population_size, mutation_rate, dataset, scaling, epochs, matingpool):
         self.input_shape = input_shape
         self.output_size = output_shape
         self.dataset = dataset
@@ -21,6 +29,7 @@ class LonelyGA:
         self.matingpool = matingpool
         self.population = [LonelyDNA(initial_max_nodes, activation, optimizer, loss, mutation_rate) for i in range(self.population_size)]
         self.evolution()
+
 
     def evolution(self):
         while True:
--- src/GARunner.py
+++ src/GARunner.py
@@ -1,3 +1,5 @@
+from threading import Thread
+
 from src.GA.SimpleGA import SimpleGA
 from src.GA.LonelyGA import LonelyGA
 from src.Enums.ActivationEnum import Activation
@@ -7,6 +9,8 @@ from src.Enums.OptimizerEnum import Optimizer
 import tensorflow as tf
 from tensorflow.keras import datasets
 import gc
+import sys
+import time
 
 
 # CONSTANTS
@@ -16,6 +20,7 @@ OUTPUT_SHAPE = 10
 SCALING = 255.0
 DATASET = datasets.mnist.load_data()
 EPOCHS = 5
+MAX_RUNTIME = 60
 
 # Hyper parameters
 ACTIVATION_FUNCTION = Activation.relu
@@ -25,21 +30,67 @@ OPTIMIZER = Optimizer.Adam
 
 # GA parameters
 POPULATION_SIZE = 10
-MATINGPOOL = 10  # Must be between 0 and POPULATION_SIZE
+MATINGPOOL = 10  # Must be between 1 and POPULATION_SIZE
 MUTATION_RATE = 0.3  # Must be between 0 and 1
 
-print("GA Started")
-gc.enable()
-ga = LonelyGA(input_shape=INPUT_SHAPE,
-              output_shape=OUTPUT_SHAPE,
-              initial_max_nodes=INITIAL_MAX_NODES,
-              activation=ACTIVATION_FUNCTION,
-              optimizer=OPTIMIZER,
-              loss=LOSS_FUNCTION,
-              population_size=POPULATION_SIZE,
-              mutation_rate=MUTATION_RATE,
-              scaling=SCALING,
-              dataset=DATASET,
-              epochs=EPOCHS,
-              matingpool=MATINGPOOL)
 
+def initialize_tf():
+    (x_train, y_train), (x_test, y_test) = DATASET
+    x_train, x_test = x_train / 255.0, x_test / 255.0
+
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Flatten(input_shape=(28, 28)),
+        tf.keras.layers.Dense(10, activation='softmax')
+    ])
+
+    model.compile(optimizer=Optimizer.Adam.name,
+                  loss=Loss.sparse_categorical_crossentropy.name,
+                  metrics=['accuracy'])
+
+    model.fit(x_train[:10], y_train[:10], epochs=1, verbose=0)
+
+
+ga = LonelyGA()
+
+
+def lonely_ga():
+    global ga
+    LonelyGA.start(self=ga,
+                   input_shape=INPUT_SHAPE,
+                   output_shape=OUTPUT_SHAPE,
+                   initial_max_nodes=INITIAL_MAX_NODES,
+                   activation=ACTIVATION_FUNCTION,
+                   optimizer=OPTIMIZER,
+                   loss=LOSS_FUNCTION,
+                   population_size=POPULATION_SIZE,
+                   mutation_rate=MUTATION_RATE,
+                   scaling=SCALING,
+                   dataset=DATASET,
+                   epochs=EPOCHS,
+                   matingpool=MATINGPOOL)
+
+
+gc.enable()
+initialize_tf()
+
+
+t = Thread(target=lonely_ga)
+t.start()
+
+tc_switch = True
+tc1 = time.perf_counter()
+tc2 = 0
+time_elapsed = 0
+
+while(True):
+    if time_elapsed >= MAX_RUNTIME:
+        print("exiting")
+        sys.exit()
+    if tc_switch:
+        tc2 = time.perf_counter()
+        time_elapsed += tc2-tc1
+        tc_switch = False
+    if not tc_switch:
+        tc1 = time.perf_counter()
+        time_elapsed += tc1-tc2
+        tc_switch = True
